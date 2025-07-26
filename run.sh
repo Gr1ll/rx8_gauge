eceafbd709ce1b62544c0bfffbefbd757db991ac
Cyril Kurmann <cyril.kurmann@gmail.com>
2025-07-26 17:57:07 +0200

    fix screipt

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+while [ ! -e /dev/fb0 ]; do
+  echo "Waiting for /dev/fb0 device..."
+  sleep 1
+done
+
+echo "/dev/fb0 found"
+
 cd /home/grill/rx8_gauge
 ./target/release/rx8_gauge
